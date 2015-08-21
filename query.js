004f273fd6973fdba6ce9467efc427e7359bea49
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-08-21 15:00:46 +0300

    Sovellusohje-urlin parsinta ankkureiden mukaan

--- query.js
+++ query.js
@@ -8,7 +8,7 @@
     var url;
     query = query.trim();
 
-    // Melinda-idt voi syöttää pilkulla tai välilyönnillä erotettuna
+    // Comma or whitespace can be used as a separator when inputting multiple ID's
     if (query.indexOf(',') > -1 ) {
       idArray = query.split(',');
     } else if (query.indexOf(' ') > -1 ) {
@@ -25,7 +25,7 @@
     return urlArray;
   }
 
-  // Parsitaan kenttäkoodit kolminumeroisiksi
+  // MARC field codes are parsed into three-digit format (e.g. '10' -> '010')
   function parseMARCField(field) {
     if (field.length == 1) {
       field = '00' + field;
@@ -92,47 +92,63 @@
 
   function parseSovellusohjeUrl(field) {
     var fieldNumber = Number(field);
-      if (fieldNumber === 0) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28195289';
-    } else if (fieldNumber > 0 && fieldNumber <= 6) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28195301';
-    } else if (fieldNumber === 7) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28195745';
-    } else if (fieldNumber === 8) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28195750';
-    } else if (fieldNumber >= 10 && fieldNumber <= 49) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28195782';
-    } else if (fieldNumber >= 50 && fieldNumber < 90) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28201092';
-    } else if (fieldNumber >= 100 && fieldNumber <= 130) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28201245';
-    } else if (fieldNumber >= 210 && fieldNumber < 250) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28201255';
-    } else if (fieldNumber >= 250 && fieldNumber <= 270) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28201803';
-    } else if (fieldNumber >= 300 && fieldNumber < 400) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28201880';
-    } else if (fieldNumber >= 400 && fieldNumber < 500) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202275';
-    } else if (fieldNumber >= 500 && fieldNumber < 540) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202285';
-    } else if (fieldNumber >= 530 && fieldNumber < 600) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202292';
-    } else if (fieldNumber >= 600 && fieldNumber < 700) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202299';
-    } else if (fieldNumber >= 700 && fieldNumber < 760) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202301';
-    } else if (fieldNumber >= 760 && fieldNumber < 790) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202303';
-    } else if (fieldNumber >= 800 && fieldNumber <= 830) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202305';
-    } else if (fieldNumber >= 841 && fieldNumber < 890) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28202307';
-    } else if (fieldNumber >= 900) {
-      return 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=28203469';
-    } else {
-      unrecognizedValue();
+    var wikiRoot = 'https://wiki.helsinki.fi/pages/viewpage.action?pageId=';
+    var sovellusohjeUrl = '';
+
+    var wikiPages = [
+      {'field': '000', 'firstcode': 0, 'lastcode': 0, 'pageId': '28195289', 'anchor': false, 'anchoredTags': false},
+      {'field': '001-006', 'firstcode': 1, 'lastcode': 6, 'pageId': '28195301', 'anchor': false, 'anchoredTags': false},
+      {'field': '007', 'firstcode': 7, 'lastcode': 7, 'pageId': '28195745', 'anchor': false, 'anchoredTags': false},
+      {'field': '008', 'firstcode': 8, 'lastcode': 8, 'pageId': '28195750', 'anchor': false, 'anchoredTags': false},
+      {'field': '01X-04X', 'firstcode': 10, 'lastcode': 49, 'pageId': '28195782', 'anchor': '#id-6.Numero-jakoodikentät(01X-04X)-', 
+      'anchoredTags': ['010','013','015','016','017','018','019','020','022','024','025','026','027','028','030','031','032','033',
+      '034','035','036','037','038','039','040','041','042','043','044','045','046','047','048','049']},
+      {'field': '05X-08X', 'firstcode': 50, 'lastcode': 89, 'pageId': '28201092', 'anchor': false, 'anchoredTags': false},
+      {'field': '1XX', 'firstcode': 100, 'lastcode': 199, 'pageId': '28201245', 'anchor': '#id-8.Pääkirjauskentät(1XX)-',
+      'anchoredTags': ['100', '110', '111', '130']},
+      {'field': '20X-24X', 'firstcode': 200, 'lastcode': 249, 'pageId': '28201255', 'anchor': '#id-9.Nimeke-janimekkeeseenliittyvätkentät(20X-24X)-',
+      'anchoredTags': ['210','222','240','242','243','245','246','247']},
+      {'field': '250-270', 'firstcode': 250, 'lastcode': 270, 'pageId': '28201803', 'anchor': '#id-10.Julkaisu-jajakelutietojenjne.kentät(250-270)-',
+      'anchoredTags': ['250','254','255','256','257','260','263','264','270']},
+      {'field': '3XX', 'firstcode': 300, 'lastcode': 399, 'pageId': '28201880', 'anchor': '#id-11.Fyysisenkuvailunjne.kentät(3XX)-',
+      'anchoredTags': ['300','306','307','310','321','336','337','338','340','342','343','344','345','346','347','351','352','355','357',
+      '362','363','365','366','370','377','380','381','382','383','384','385','386','388']},
+      {'field': '4XX', 'firstcode': 400, 'lastcode': 499, 'pageId': '28202275', 'anchor': false, 'anchoredTags': false},
+      {'field': '50x-53X', 'firstcode': 500, 'lastcode': 535, 'pageId': '28202285', 'anchor': '#id-13.Huomautuskentät,osa1(50X-53X)-',
+      'anchoredTags': ['500','501','502','504','505','506','507','508','509','510','511','513','514','515','516','518','520','521','522',
+      '524','525','526','530','533','534','535']},
+      {'field': '53X-59X', 'firstcode': 536, 'lastcode': 599, 'pageId': '28202292', 'anchor': '#id-14.Huomautuskentät,osa2(53X-59X)-',
+      'anchoredTags': ['536','538','540','541','542','544','545','546','547','550','552','555','556','561','562','563','565','567','579',
+      '580','581','583','584','585','586','588','59x']},
+      {'field': '6XX', 'firstcode': 600, 'lastcode': 699, 'pageId': '28202299', 'anchor': '#id-15.Asiasanakentät(6XX)-',
+      'anchoredTags': ['600','610','611','630','648','650','651','653','654','655','656','657','658','662']},
+      {'field': '70X-75X', 'firstcode': 700, 'lastcode': 759, 'pageId': '28202301', 'anchor': '#id-16.Lisäkirjauskentät(70X-75X)-', 
+      'anchoredTags': ['700','710','711','720','730','740','751','752','753','754']},
+      {'field': '76X-79X', 'firstcode': 760, 'lastcode': 799, 'pageId': '28202303', 'anchor': '#id-17.Linkkikentät(76X-79X)-',
+      'anchoredTags': ['760','762','765','767','770','772','773','774','775','776','777','780','785','786','787','790']},
+      {'field': '80X-830', 'firstcode': 800, 'lastcode': 830, 'pageId': '28202305', 'anchor': '#id-18.Sarjalisäkirjauskentät(80X-830)-',
+      'anchoredTags': ['800','810','811','830']},
+      {'field': '841-88X', 'firstcode': 841, 'lastcode': 889, 'pageId': '28202307', 'anchor': '#id-19.Varasto-jasijainti-ym.tietojenkentät(841-88X)-',
+      'anchoredTags': ['850','852','856','880','882','883','884','886','887']},
+      {'field': '9XX', 'firstcode': 900, 'lastcode': 999, 'pageId': '28203469', 'anchor': '#id-20.Suomalaisetkentät(9XX)-',
+      'anchoredTags': ['900','901','902','903','904','905','906','907','908','910','911','930','931','932','933','935','940','960','971','972']}
+    ];
+
+    if (fieldNumber >= 590 && fieldNumber <= 599) {
+      field = '59x';
     }
+
+    for (var i = 0; sovellusohjeUrl.length < 1; i++) {
+      if (fieldNumber >= wikiPages[i].firstcode && fieldNumber <= wikiPages[i].lastcode) {
+        sovellusohjeUrl = wikiRoot + wikiPages[i].pageId;
+        if (wikiPages[i].anchor && wikiPages[i].anchoredTags.indexOf(field) > -1) {
+          sovellusohjeUrl += wikiPages[i].anchor;
+          sovellusohjeUrl += field;
+        }
+      }
+    }
+
+    return sovellusohjeUrl;
   }
 
   function redirect(parsedUrl) {
@@ -171,13 +187,13 @@
     }
   }
 
-  // Lomakkeen lähetys painamalla hae-nappia
+  // Send the form by pressing the button
 
   document.getElementById('sendButton').addEventListener('click', processForm);
 
-  // Lomakkeen lähetys painamalla enteriä
+  // Send the form by hitting enter
 
-  // Enterin painaminen kun hakulaatikko on valittu
+  // Pressing enter when the search box is highlighted
 
   document.getElementById('querystring').addEventListener('keypress', function(event) {
     if (event.keyCode == 13) {
@@ -185,7 +201,7 @@
     }
   });
 
-  // Enterin painaminen kohdistusvalintojen kohdalla (esim. jos ensin täytetään hakuehto ja sitten kohdistus)
+  // Pressing enter when one of the radio buttons is highlighted
 
   var elements = document.getElementsByClassName('teksti');
 
